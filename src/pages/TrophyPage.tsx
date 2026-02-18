b4e0add11984e6102a2d7ced85d27210be18b74a
PerfectNich <erreivax11@gmail.com>
2026-02-18 15:54:21 +0100

    delete

--- src/pages/TrophyPage.tsx
+++
@@ -1,3 +0,0 @@
-import { PageHeader } from "../components/common/PageHeader";
-import { Section } from "../components/common/Section";
-import { SectionTitle } from "../components/common/SectionTitle";
