dbaa9a666ad78eb714a5681fa4eded3842c56096
PerfectNich <erreivax11@gmail.com>
2026-02-20 10:19:10 +0100

    tx

--- src/pages/CalendarPage.tsx
+++ src/pages/CalendarPage.tsx
@@ -1,7 +1,6 @@
 import { PageHeader } from "../components/common/PageHeader";
 import { Section } from "../components/common/Section";
 import { SectionTitle } from "../components/common/SectionTitle";
-import { TwitterWidget } from "../components/common/TwitterWidget";
 
 export function CalendarPage() {
   return (
