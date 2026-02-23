189d2c0b273ea326550816026c6fab78644b179e
PerfectNich <erreivax11@gmail.com>
2026-02-23 19:55:31 +0100

    izq

--- src/pages/PalmaresPage.tsx
+++ src/pages/PalmaresPage.tsx
@@ -30,141 +30,21 @@ export function PalmaresPage() {
 
         <SectionTitle>Assetto Corsa Competizione</SectionTitle>
         <ul className="list-disc list-inside space-y-1">
-          <li>
-            {medal(1)} <span className="font-semibold" style={{ color: "var(--color-lynx-orange-light)" }}>
-              Resistencia 4h Barcelona | Benéfica de La Cueva
-            </span>{" "}
-            Kiko Ribes | Xavier Sobrerroca – 2023
-            <span className="ml-2 inline-block w-4 h-4">
-              <img
-                src={assetUrl("/marcas/porsche.png")}
-                alt="Porsche"
-                className="w-full h-full object-contain"
-              />
-            </span>
-          </li>
-          <li>
-            {medal(2)} <span className="font-semibold" style={{ color: "var(--color-lynx-orange-light)" }}>
-              Resistencia 8h Suzuka | CER-V
-            </span>{" "}
-            Dani Gala | Juan Serrano | Jorge Pola – 2022
-            <span className="ml-2 inline-block w-4 h-4">
-              <img
-                src={assetUrl("/marcas/aston.png")}
-                alt="Aston"
-                className="w-full h-full object-contain"
-              />
-            </span>
-          </li>
-          <li>
-            {medal(3)} <span className="font-semibold" style={{ color: "var(--color-lynx-orange-light)" }}>
-              Resistencia 9h Kyalami | CER-V
-            </span>{" "}
-            Kiko Ribes | Juan Serrano – 2022
-            <span className="ml-2 inline-block w-4 h-4">
-              <img
-                src={assetUrl("/marcas/aston.png")}
-                alt="Aston"
-                className="w-full h-full object-contain"
-              />
-            </span>
-          </li>
-          <li>
-            {medal(7)} <span className="font-semibold" style={{ color: "var(--color-lynx-orange-light)" }}>
-              P7 Resistencia 24h Nordschleife | Pitskill
-            </span>{" "}
-            Jordi Capdevila | Luis Ungo | Juan Serrano | Xavier Sobrerroca | Albert Gombau – 2025
-            <span className="ml-2 inline-block w-4 h-4">
-              <img
-                src={assetUrl("/marcas/ferrari.png")}
-                alt="Ferrari"
-                className="w-full h-full object-contain"
-              />
-            </span>
-          </li>
-          <li>
-            {medal(1)} <span className="font-semibold" style={{ color: "var(--color-lynx-orange-light)" }}>
-              Campeonato CNE II VRGirona
-            </span>{" "}
-            Juan Serrano | Josh Mopar | Xavier Sobrerroca – 2023
-            <span className="ml-2 inline-block w-4 h-4">
-              <img
-                src={assetUrl("/marcas/aston.png")}
-                alt="Aston"
-                className="w-full h-full object-contain"
-              />
-            </span>
-          </li>
-          <li>
-            {medal(4)} <span className="font-semibold" style={{ color: "var(--color-lynx-orange-light)" }}>
-              Endurance 4h Titanium Red
-            </span>{" "}
-            Jordi Capdevila | Francisco Sierra – 2025
-            <span className="ml-2 inline-block w-4 h-4">
-              <img
-                src={assetUrl("/marcas/ferrari.png")}
-                alt="Ferrari"
-                className="w-full h-full object-contain"
-              />
-            </span>
-          </li>
+          <li>{medal(1)} <span className="font-semibold" style={{ color: "var(--color-lynx-orange-light)" }}>Resistencia 4h Barcelona | Benéfica de La Cueva</span> Kiko Ribes | Xavier Sobrerroca – 2023 <span className="ml-2 inline-block w-4 h-4"><img src={assetUrl("/marcas/porsche.png")} alt="Porsche" className="w-full h-full object-contain" /></span></li>
+          <li>{medal(2)} <span className="font-semibold" style={{ color: "var(--color-lynx-orange-light)" }}>Resistencia 8h Suzuka | CER-V</span> Dani Gala | Juan Serrano | Jorge Pola – 2022 <span className="ml-2 inline-block w-4 h-4"><img src={assetUrl("/marcas/aston.png")} alt="Aston" className="w-full h-full object-contain" /></span></li>
+          <li>{medal(3)} <span className="font-semibold" style={{ color: "var(--color-lynx-orange-light)" }}>Resistencia 9h Kyalami | CER-V</span> Kiko Ribes | Juan Serrano – 2022 <span className="ml-2 inline-block w-4 h-4"><img src={assetUrl("/marcas/aston.png")} alt="Aston" className="w-full h-full object-contain" /></span></li>
+          <li>{medal(7)} <span className="font-semibold" style={{ color: "var(--color-lynx-orange-light)" }}>P7 Resistencia 24h Nordschleife | Pitskill</span> Jordi Capdevila | Luis Ungo | Juan Serrano | Xavier Sobrerroca | Albert Gombau – 2025 <span className="ml-2 inline-block w-4 h-4"><img src={assetUrl("/marcas/ferrari.png")} alt="Ferrari" className="w-full h-full object-contain" /></span></li>
+          <li>{medal(1)} <span className="font-semibold" style={{ color: "var(--color-lynx-orange-light)" }}>Campeonato CNE II VRGirona</span> Juan Serrano | Josh Mopar | Xavier Sobrerroca – 2023 <span className="ml-2 inline-block w-4 h-4"><img src={assetUrl("/marcas/aston.png")} alt="Aston" className="w-full h-full object-contain" /></span></li>
+          <li>{medal(4)} <span className="font-semibold" style={{ color: "var(--color-lynx-orange-light)" }}>Endurance 4h Titanium Red</span> Jordi Capdevila | Francisco Sierra – 2025 <span className="ml-2 inline-block w-4 h-4"><img src={assetUrl("/marcas/ferrari.png")} alt="Ferrari" className="w-full h-full object-contain" /></span></li>
         </ul>
         <hr className="border-lynx-border my-6" />
 
         <SectionTitle>iRacing</SectionTitle>
         <ul className="list-disc list-inside space-y-1">
-          <li>
-            {medal(2)} <span className="font-semibold" style={{ color: "var(--color-lynx-orange-light)" }}>
-              Resistencia 24h Daytona
-            </span>{" "}
-            Jordi Capdevila | Jesús Jiménez | Francisco Sierra | Luis Ungo | Xavier Sobrerroca – 2026
-            <span className="ml-2 inline-block w-4 h-4">
-              <img
-                src={assetUrl("/marcas/porsche.png")}
-                alt="Porsche"
-                className="w-full h-full object-contain"
-              />
-            </span>
-          </li>
-          <li>
-            {medal(4)} <span className="font-semibold" style={{ color: "var(--color-lynx-orange-light)" }}>
-              P4 Resistencia 6h Watkins Glen
-            </span>{" "}
-            Albert Gombau | Lalo Sanchez – 2024
-            <span className="ml-2 inline-block w-4 h-4">
-              <img
-                src={assetUrl("/marcas/acura.png")}
-                alt="Acura"
-                className="w-full h-full object-contain"
-              />
-            </span>
-          </li>
-          <li>
-            {medal(3)} <span className="font-semibold" style={{ color: "var(--color-lynx-orange-light)" }}>
-              Resistencia 12h Mount Panorama
-            </span>{" "}
-            Angel Alvarado | Albert Gombau | Nacho Jarrín - 2026
-            <span className="ml-2 inline-block w-4 h-4">
-              <img
-                src={assetUrl("/marcas/mclaren.png")}
-                alt="Mclaren"
-                className="w-full h-full object-contain"
-              />
-            </span>
-          </li>
-          <li>
-            <span className="font-semibold" style={{ color: "var(--color-lynx-orange-light)" }}>
-              P4 Resistencia 6h Sebring
-            </span>{" "}
-            Jordi Capdevila | Francisco Sierra - 2026
-            <span className="ml-2 inline-block w-4 h-4">
-              <img
-                src={assetUrl("/marcas/porsche.png")}
-                alt="Porsche"
-                className="w-full h-full object-contain"
-              />
-            </span>
-          </li>
+          <li>{medal(2)} <span className="font-semibold" style={{ color: "var(--color-lynx-orange-light)" }}>Resistencia 24h Daytona</span> Jordi Capdevila | Jesús Jiménez | Francisco Sierra | Luis Ungo | Xavier Sobrerroca – 2026 <span className="ml-2 inline-block w-4 h-4"><img src={assetUrl("/marcas/porsche.png")} alt="Porsche" className="w-full h-full object-contain" /></span></li>
+          <li>{medal(4)} <span className="font-semibold" style={{ color: "var(--color-lynx-orange-light)" }}>P4 Resistencia 6h Watkins Glen</span> Albert Gombau | Lalo Sanchez – 2024 <span className="ml-2 inline-block w-4 h-4"><img src={assetUrl("/marcas/acura.png")} alt="Acura" className="w-full h-full object-contain" /></span></li>
+          <li>{medal(3)} <span className="font-semibold" style={{ color: "var(--color-lynx-orange-light)" }}>Resistencia 12h Mount Panorama</span> Angel Alvarado | Albert Gombau | Nacho Jarrín - 2026 <span className="ml-2 inline-block w-4 h-4"><img src={assetUrl("/marcas/mclaren.png")} alt="Mclaren" className="w-full h-full object-contain" /></span></li>
+          <li><span className="font-semibold" style={{ color: "var(--color-lynx-orange-light)" }}>P4 Resistencia 6h Sebring</span> Jordi Capdevila | Francisco Sierra - 2026 <span className="ml-2 inline-block w-4 h-4"><img src={assetUrl("/marcas/porsche.png")} alt="Porsche" className="w-full h-full object-contain" /></span></li>
         </ul>
       </Section>
     </>
